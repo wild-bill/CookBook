a9c4c99cba5b4eae0fcee8ca928a39739b662dc2
Will <william.kryjak@gmail.com>
2021-08-13 16:39:15 -0400

    Through django templates page of tutorial

--- cookbook/views.py
+++ cookbook/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Recipe
+from django.utils import timezone
 
 # Create your views here.
+
+def post_list(request):
+    recipes = Recipe.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'cookbook/post_list.html', {'recipes': recipes})
+
