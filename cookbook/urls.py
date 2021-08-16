8ded4d41c4ee412cca59123a8a1dd199d81e8298
Will <william.kryjak@gmail.com>
2021-08-15 22:21:54 -0400

    added views to create/edit recipes from inside the site

--- cookbook/urls.py
+++ cookbook/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path('',views.post_list, name='post_list'),
+    path('recipe/<int:pk>/', views.post_detail, name='post_detail'),
+    path('recipe/new/', views.post_new, name = 'post_new'),
+    path('recipe/<int:pk>/edit/', views.post_edit, name='post_edit'),
 ]
--- cookbook/views.py
+++ cookbook/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
 from .models import Recipe
 from django.utils import timezone
+from django.shortcuts import render, get_object_or_404, redirect
+from .forms import RecipeForm
 
 # Create your views here.
 
@@ -8,3 +9,36 @@ def post_list(request):
     recipes = Recipe.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'cookbook/post_list.html', {'recipes': recipes})
 
+def post_detail(request, pk):
+    recipe = get_object_or_404(Recipe, pk=pk)
+    return render(request, 'cookbook/post_detail.html',{'recipe':recipe})
+
+def post_new(request):
+    if request.method == "POST":
+        form = RecipeForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            #post.recipe_yield = request.recipe_yield
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk = post.pk)
+    else:
+        form = RecipeForm()
+    
+    return render(request, 'cookbook/post_edit.html', {'form': form})
+
+def post_edit(request, pk):
+    post = get_object_or_404(Recipe, pk=pk)
+    if request.method == "POST":
+        form = RecipeForm(request.POST, instance = post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = RecipeForm(instance=post)
+    
+    return render(request, 'cookbook/post_edit.html', {'form': form})
